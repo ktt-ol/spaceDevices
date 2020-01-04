250568578a40a84a03367cb1f564a5e8978fd88e
Holger Cremer <HolgerCremer@gmail.com>
2020-01-04 19:02:40 +0100

    adds a docker image to build the binary

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage $0 (build-linux|test-sync|sync)"
+    echo "Usage $0 (build-linux|docker-image|docker-build|test-sync|sync)"
 }
 
 if [ "$1" == "" ]; then
@@ -15,6 +15,12 @@ while (( "$#" )); do
         build-linux)
             env GOOS=linux GOARCH=amd64 go build cmd/spaceDevices/spaceDevices.go
             env GOOS=linux GOARCH=amd64 go build cmd/unkownDevices/listUnkown.go
+            ;;
+        docker-image)
+            docker build -t space-devices-build docker/
+            ;;
+        docker-build)
+            docker run --rm -it -v $(pwd):/go/src/spaceDevices space-devices-build dep ensure -v -vendor-only && ./do.sh build-linux
             ;;
         test-sync)
             rsync -n -avzi --delete spaceDevices listUnkown webUI macVendorDb.csv root@spacegate:/home/status/spaceDevices2/
---
+++ docker/README.md
@@ -0,0 +1,15 @@
+# Build with docker
+
+Use the `do.sh` script file in the top directory.
+
+## Build the docker image
+
+```shell script
+./do.sh docker-image
+```
+
+## Build binary
+
+```shell script
+./do.sh docker-build
+```
